2a27e8c0e5fb15ab848866a261c17e11c592eb67
TheOmegaWolf <theomegawolf08@gmail.com>
2024-10-27 18:26:16 -0500

    Forgot Password done!

--- app/src/main/java/com/example/ikuzo/Login.java
+++ app/src/main/java/com/example/ikuzo/Login.java
@@ -1,16 +1,23 @@
 package com.example.ikuzo;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,12 +27,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
     private EditText editTextLoginEmail, editTextLoginPwd;
     private ProgressBar progressBar;
     private FirebaseAuth authProfile;
-
+    private static final String TAG = "Login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +52,28 @@ public class Login extends AppCompatActivity {
         progressBar = findViewById(R.id.progressBar);
 
         authProfile = FirebaseAuth.getInstance();
+
+        Button buttonForgotPassword = findViewById(R.id.button_forgot_password);
+        buttonForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Login.this, "You can reset your password now!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Login.this, ForgotPasswordActivity.class));
+            }
+        });
+        // Show or Hide Password using Eye Icon
+        ImageView imageViewShowHidePwd = findViewById(R.id.imageView_show_hide_pwd);
+        imageViewShowHidePwd.setImageResource(R.drawable.ic_hide_pwd);
+        imageViewShowHidePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editTextLoginPwd.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance())){
+                    editTextLoginPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }else{
+                    editTextLoginPwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+            }
+        });
 
         // Login User
         Button buttonLogin = findViewById(R.id.button_login);
@@ -76,11 +108,70 @@ public class Login extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
                     Toast.makeText(Login.this, "User has logged in!", Toast.LENGTH_SHORT).show();
+
+                    // get current user instance
+                    FirebaseUser firebaseUser = authProfile.getCurrentUser();
+
+
+                    if(firebaseUser.isEmailVerified()){
+                        Toast.makeText(Login.this, "You are logged in now!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        firebaseUser.sendEmailVerification();
+                        authProfile.signOut();
+                        showAlertDialog();
+                    }
+
                 } else {
-                    Toast.makeText(Login.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    try {
+                        throw task.getException();
+                    }catch(FirebaseAuthInvalidUserException e){
+                        editTextLoginEmail.setError("User does not exist or is no longer valid, please register.");
+                        editTextLoginEmail.requestFocus();
+                    }catch(FirebaseAuthInvalidCredentialsException e){
+                        editTextLoginEmail.setError("Invalid Credentials, kindly re-enter");
+                        editTextLoginEmail.requestFocus();
+                    }catch(Exception e){
+                        Log.e(TAG, e.getMessage());
+                        Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
                 }
                 progressBar.setVisibility(View.GONE);
             }
         });
+    }
+    private void showAlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+        builder.setTitle("Email Not Verified");
+        builder.setMessage("Please verify your email address, cannot proceed without verification");
+
+        //open email apps
+        builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_APP_EMAIL);
+                intent.addFlags(intent.FLAG_ACTIVITY_NEW_TASK); // To open email app in new window
+                startActivity(intent);
+            }
+        });
+        // Create Alert Dialog
+        AlertDialog alertDialog = builder.create();
+
+        // Show alert dialog
+        alertDialog.show();
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(authProfile.getCurrentUser() != null){
+            Toast.makeText(Login.this, "Already logged in!", Toast.LENGTH_SHORT).show();
+
+            // Start the User Profile Activity
+//            startActivity(new Intent(Login.this, UserProfileActivity.class));
+//            finish();
+        }else{
+            Toast.makeText(Login.this, "You can login now!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
